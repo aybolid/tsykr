08f2563ee4a029bd906adbab6a2d58651f8a483b
matth <matthewiva13@gmail.com>
2025-03-25 18:42:02 +0200

    eval let

--- src/parser/ast/stmt_let.rs
+++ src/parser/ast/stmt_let.rs
@@ -1,5 +1,7 @@
+use std::{cell::RefCell, rc::Rc};
+
 use crate::{
-    eval::Eval,
+    eval::{Environment, Eval, EvalError, ExecutionEnvironment, Value, VOID},
     lexer::{Token, TokenKind},
 };
 
@@ -48,18 +50,20 @@ impl Node for LetStatement {
 }
 
 impl Eval for LetStatement {
-    fn eval(
-        &self,
-        _env: std::rc::Rc<std::cell::RefCell<crate::eval::ExecutionEnvironment>>,
-    ) -> Result<std::rc::Rc<crate::eval::Value>, crate::eval::EvalError> {
-        todo!()
+    fn eval(&self, env: Rc<RefCell<ExecutionEnvironment>>) -> Result<Rc<Value>, EvalError> {
+        let to_store = self.value.eval(Rc::clone(&env))?;
+        let name = self.identifier.token_literal();
+
+        env.borrow_mut().set(name, to_store);
+
+        Ok(VOID.rc())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::lexer::Position;
+    use crate::{eval::Environment, lexer::Position};
 
     #[test]
     fn test_let_statement() {
@@ -74,5 +78,25 @@ mod tests {
 
         assert_eq!(stmt.token_literal(), token.literal());
         assert_eq!(stmt.to_string(), "let who_cares = 69");
+    }
+
+    #[test]
+    fn test_let_eval() {
+        let token = Token::new(TokenKind::Let, Position(0, 0));
+        let ident = Identifier::new(Token::new(
+            TokenKind::Identifier("who_cares".to_string()),
+            Position(0, 0),
+        ));
+        let int = Expression::new_integer(Token::new(TokenKind::Integer(69), Position(0, 0)));
+
+        let stmt = LetStatement::new(token, ident, Box::new(int));
+
+        let env = ExecutionEnvironment::new_global();
+        let result = stmt.eval(Rc::clone(&env));
+        let stored = env.borrow().get("who_cares");
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), VOID.rc());
+        assert_eq!(stored, Some(Value::new_integer(69)))
     }
 }
